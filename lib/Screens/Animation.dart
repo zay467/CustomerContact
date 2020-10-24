f7df06faba4bdc513961a2f8e20f477e987fbf2c
Zay Min Maw <zayminmaw77@gmail.com>
2020-10-24 14:30:01 +0630

    Edit Animation

--- lib/Screens/Animation.dart
+++ lib/Screens/Animation.dart
@@ -7,67 +7,63 @@ class MyAnimation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+        backgroundColor: Color(0xFF00D8E7),
         body: Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Center(
-        child: Container(
-          width: 500,
-          height: 500,
-          child: Card(
-    shadowColor: Colors.black,
-    color: Colors.teal,
-
-
-
-    child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-
-                ColorizeAnimatedTextKit(
-                  isRepeatingAnimation: true,
-                  text: [
-                    "Created By",
-                  ],
-                  pause: Duration(milliseconds: 200),
-                  speed: Duration(milliseconds: 200),
-                  textStyle: TextStyle(
-                      fontSize: 20.0,
-                      fontFamily: "Pacifico",
-                      fontWeight: FontWeight.bold),
-                  colors: [
-                    Colors.red,
-                    Colors.orange,
-                    Colors.yellow,
-                    Colors.green,
-                    Colors.blue,
-                    Colors.purple
-                  ],
+            padding: EdgeInsets.all(8.0),
+            child: Center(
+              child: Container(
+                // width: 500,
+                // height: MediaQuery.of(context).size.height - 40,
+                child: Card(
+                  shadowColor: Colors.black,
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        ColorizeAnimatedTextKit(
+                          isRepeatingAnimation: true,
+                          text: [
+                            "Created By",
+                          ],
+                          pause: Duration(milliseconds: 200),
+                          speed: Duration(milliseconds: 200),
+                          textStyle: TextStyle(
+                              fontSize: 20.0,
+                              fontFamily: "Pacifico",
+                              fontWeight: FontWeight.bold),
+                          colors: [
+                            Colors.red,
+                            Colors.orange,
+                            Colors.yellow,
+                            Colors.green,
+                            Colors.blue,
+                            Colors.purple
+                          ],
+                        ),
+                        TyperAnimatedTextKit(
+                          isRepeatingAnimation: false,
+                          text: ["Generation 2K"],
+                          speed: Duration(milliseconds: 250),
+                          alignment: Alignment.center,
+                          textStyle:
+                              TextStyle(fontSize: 50.0, fontFamily: "Stat"),
+                          onFinished: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Home()));
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
                 ),
-                TyperAnimatedTextKit(
-
-                  isRepeatingAnimation: false,
-                  text: ["Generation 2K"],
-                  speed: Duration(milliseconds: 250),
-                  alignment: Alignment.center,
-                  textStyle: TextStyle(fontSize: 50.0, fontFamily: "Stat"),
-                  onFinished: () {
-                    Navigator.push(
-                        context, MaterialPageRoute(builder: (context) => Home()));
-                  },
-                ),
-              ],
-            ),
-
-
-    shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.circular(30.0),
-
-    ),
-        ),
-    ),
-      )
-    ));
+              ),
+            )));
   }
 }
