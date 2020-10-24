33123f0aa52323475127540a4bacfa424c45fb2e
Zinlin49052002 <56996330+Zinlin49052002@users.noreply.github.com>
2020-10-24 14:17:16 +0630

    add file

--- lib/Screens/Animation.dart
+++ lib/Screens/Animation.dart
@@ -7,44 +7,67 @@ class MyAnimation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
         body: Padding(
       padding: EdgeInsets.all(8.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          ColorizeAnimatedTextKit(
-            isRepeatingAnimation: true,
-            text: [
-              "Created By",
-            ],
-            speed: Duration(milliseconds: 200),
-            textStyle: TextStyle(
-                fontSize: 20.0,
-                fontFamily: "Pacifico",
-                fontWeight: FontWeight.bold),
-            colors: [
-              Colors.red,
-              Colors.orange,
-              Colors.yellow,
-              Colors.green,
-              Colors.blue,
-              Colors.purple
-            ],
-          ),
-          TyperAnimatedTextKit(
-            isRepeatingAnimation: false,
-            text: ["Generation 2K"],
-            speed: Duration(milliseconds: 250),
-            alignment: Alignment.center,
-            textStyle: TextStyle(fontSize: 50.0, fontFamily: "Stat"),
-            onFinished: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => Home()));
-            },
-          ),
-        ],
-      ),
+      child: Center(
+        child: Container(
+          width: 500,
+          height: 500,
+          child: Card(
+    shadowColor: Colors.black,
+    color: Colors.teal,
+
+
+
+    child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+
+                ColorizeAnimatedTextKit(
+                  isRepeatingAnimation: true,
+                  text: [
+                    "Created By",
+                  ],
+                  pause: Duration(milliseconds: 200),
+                  speed: Duration(milliseconds: 200),
+                  textStyle: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: "Pacifico",
+                      fontWeight: FontWeight.bold),
+                  colors: [
+                    Colors.red,
+                    Colors.orange,
+                    Colors.yellow,
+                    Colors.green,
+                    Colors.blue,
+                    Colors.purple
+                  ],
+                ),
+                TyperAnimatedTextKit(
+
+                  isRepeatingAnimation: false,
+                  text: ["Generation 2K"],
+                  speed: Duration(milliseconds: 250),
+                  alignment: Alignment.center,
+                  textStyle: TextStyle(fontSize: 50.0, fontFamily: "Stat"),
+                  onFinished: () {
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (context) => Home()));
+                  },
+                ),
+              ],
+            ),
+
+
+    shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(30.0),
+
+    ),
+        ),
+    ),
+      )
     ));
   }
 }
